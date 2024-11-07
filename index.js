f3436cad82eb0b6dfa85d199218fbe77bed55afc
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2024-11-07 14:03:50 -0500

    fizz buzz works

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Part 1: FIZZ BUZZ
+
+let counter = 1;
+while (counter <= 100) {
+    if (counter % 3 === 0 && counter % 5 === 0) {
+        console.log("Fizz Buzz");
+    }
+    else if (counter % 3 === 0) {
+        console.log("Fizz");
+    }
+    else if (counter % 5 === 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(counter);
+    }
+    counter++;
+}
