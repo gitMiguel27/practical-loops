fce414cbc1a994e2bcf59b6e339c6b5736ba9810
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2024-11-07 17:19:58 -0500

    second problem

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 // Part 1: FIZZ BUZZ
 
-let counter = 1;
-while (counter <= 100) {
-    if (counter % 3 === 0 && counter % 5 === 0) {
-        console.log("Fizz Buzz");
-    }
-    else if (counter % 3 === 0) {
-        console.log("Fizz");
-    }
-    else if (counter % 5 === 0) {
-        console.log("Buzz");
-    }
-    else {
-        console.log(counter);
-    }
-    counter++;
-}
+// let counter = 1;
+// while (counter <= 100) {
+//     if (counter % 3 === 0 && counter % 5 === 0) {
+//         console.log("Fizz Buzz");
+//     }
+//     else if (counter % 3 === 0) {
+//         console.log("Fizz");
+//     }
+//     else if (counter % 5 === 0) {
+//         console.log("Buzz");
+//     }
+//     else {
+//         console.log(counter);
+//     }
+//     counter++;
+// }
+
+// Part 2: PRIME TIME
