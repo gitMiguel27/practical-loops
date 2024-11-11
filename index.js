9df661b2f2c7846ad62919cf6919577d601353a6
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2024-11-11 13:53:11 -0500

    figuring out while loop

--- index.js
+++ index.js
@@ -18,3 +18,19 @@
 // }
 
 // Part 2: PRIME TIME
+
+// prime number examples: 5,7,11,13,17,19,23,29
+
+let x = 2;
+
+for (let n = 14; x < n; n++) {
+    while ((n + 1) % x > 0) {
+        x++;
+    }
+    console.log("This is the value of n + 1: " + (n + 1));
+    console.log((n + 1) + " is no longer prime because it is divisible by: " + x);
+    // if ((n + 1) % 2 === 0) {
+    //     console.log("This is not a prime number because it is divisible by 2");
+    //     break;
+    // }
+}
